9a80cb2f39d68d9556a6cfad1f6d4d8674af8251
say <say@yandex-team.ru>
2010-09-18 21:42:39 +0400

    Refactor code

--- mkvmerge.py
+++ mkvmerge.py
@@ -5,33 +5,97 @@ import os.path
 import sys
 import platform
 import re
+import copy
 
 OPT_SEPARATOR = '@'
 
+TRACK_EXTS = {
+    '.ac3': 'audio',
+    '.mp4': 'audio',
+    '.mkv': 'video',
+    '.txt': 'chapters',
+    '.srt': 'subtitles',
+}
+
+TRACK_TYPES = {
+    'audio': {
+        'valid_opts': 'lang title delay'.split(),
+        'guess_opts': 'delay title'.split(),
+    },
+    'video': {
+        'valid_opts': 'lang title'.split(),
+    },
+    'chapters': {
+        'valid_opts': 'lang'.split(),
+    },
+    'subtitles': {
+        'valid_opts': 'lang title delay'.split(),
+        'guess_opts': 'delay title'.split(),
+    },
+    'DEFAULT': {
+       'valid_opts': 'lang'.split(),
+    },
+}
+
+OPTION_DEFS = [
+    dict(name='delay', re=r'^(-?\d+)ms$'),
+    dict(name='lang', re=r'^([a-z]{3})$'),
+    dict(name='title', re=r'^(.+)$'),
+]
+
 def help():
-    print "Usage: %s %s" % (sys.argv[0], "dest_file [#options] file1[#options1] file2[#options2] ...")
+    print "Usage: %(cmd)s dest_file [%(sep)sdef_lang] file1[%(sep)soptions1] file2[%(sep)soptions2] ..." % { 'cmd': sys.argv[0], 'sep': OPT_SEPARATOR }
     sys.exit(1)
+
 
 def fail(message):
     print message
     sys.exit(1)
 
-def parse_options(sopts, track):
-    opt_defs = [
-        { 'name': 'lang', 're': r'^\w{3}$', },
-        { 'name': 'delay', 're': r'^-?\d+ms$', },
-        { 'name': 'title', 're': r'^.+$', }
-    ]
-    for s in sopts:
-        for opt_def in opt_defs:
-            if re.search(opt_def['re'], s):
-                name = opt_def['name']
-                if track.has_key(name):
-                    fail("duplicate options %s=%s (prev: %s)" % (name, s, track[name]))
-                if name == 'delay':
-                     s = re.sub(r'ms$', '', s)
-                track[name] = s
+
+def parse_options(opt_strs):
+    opts = {}
+    for s in opt_strs:
+        for opt_def in OPTION_DEFS:
+            name, regexp = opt_def['name'], opt_def['re']
+            m = re.search(regexp, s)
+            if m:
+                if opts.has_key(name):
+                    fail("duplicate options %s=%s (prev: %s)" % (name, s, opts[name]))
+                opts[name] = m.group(1)
                 break
+
+    return opts
+
+
+def guess_options(file, guess_opts):
+    opts = {}
+    if 'delay' in guess_opts:
+        m = re.search(r'DELAY\s*(\d+)', file)
+        if m:
+            opts['delay'] = m.group(1)
+
+    if 'title' in guess_opts:
+        m = re.search(r'(\d_\d)ch', file)
+        if m:
+            titles = {
+              '1_0': 'mono',
+              '2_0': 'stereo',
+              '3_2': '5.1',
+            }
+            if titles.has_key(m.group(1)):
+                opts['title'] = titles[m.group(1)]
+
+    return opts
+
+def print_options(opts):
+    if opts.has_key('title'):
+        uprint(['--track-name', '-1:' + opts['title']])
+    if opts.has_key('lang'):
+        uprint(['--language', '-1:' + opts['lang']])
+    if opts.has_key('delay'):
+        uprint(['--sync', '-1:' + opts['delay']])
+
 
 def uprint(strings):
     for s in strings:
@@ -46,60 +110,60 @@ else:
 if not uargv:
     help()
 
+
 tracks = {}
-def_lang = 'rus'
+def_opts = dict(lang = 'rus')
 dest_file = uargv[0]
 for arg in uargv[1:]:
     file_n_opts = arg.split(OPT_SEPARATOR);
     track_file = file_n_opts[0]
-    track = {
-        'file': track_file,
-    }
-    parse_options(file_n_opts[1:], track)
 
-    # global option (no file)
+    # Get track parameters
+    if track_file:
+        try:
+            ext = os.path.splitext(track_file)[1]
+            track_type = TRACK_EXTS[ext]
+        except:
+            fail("Unknown file type %s" % track_file) 
+    else:
+        track_type = 'DEFAULT'
+
+    valid_opts_list = TRACK_TYPES[track_type]['valid_opts']
+    guess_opts_list = TRACK_TYPES[track_type].get('guess_opts', None)
+
+    # Get track options
+    track_opts = parse_options(file_n_opts[1:])
+
+    # Check if all options are valid
+    for k in track_opts.keys():
+        if not k in valid_opts_list:
+            fail("Option %s not valid for track type %s" % k, track_type)
+
+    # set default options and go to next track
     if not track_file:
-        def_lang = track['lang']
+        for k, v in track_opts.items():
+            def_opts[k] = v
         continue
 
-    ext = os.path.splitext(track_file)[1]
+    # Try to guess options from file name
+    guess_opts = {}
+    if guess_opts_list:
+        guess_opts = guess_options(track_file, guess_opts_list)
 
-    if ext in ('.ac3', '.mp4'):
-        track['type'] = 'audio'
-        track['ext'] = ext
-	# auto delay
-        if not track.has_key('delay'):
-            m = re.search(r'DELAY\s*(\d+)', track_file)
-            if m:
-                track['delay'] = m.group(1)
-	# auto title
-	if not track.has_key('title'):
-	    m = re.search(r'(\d_\d)ch', track_file)
-	    if m:
-	      titles = {
-		  '1_0': 'mono',
-		  '2_0': 'stereo',
-		  '3_2': '5.1',
-	      }
-	      for (k, v) in titles.items():
-		if m.group(1) == k:
-		  track['title'] = v
-		  break
-    elif ext in ('.mkv'):
-        track['type'] = 'video'
-    elif ext in ('.mkv'):
-        track['type'] = 'video'
-    elif ext in ('.srt'):
-        track['type'] = 'subtitles'
-    elif ext in ('.txt'):
-        track['type'] = 'chapters'
-    else:
-        fail("Unknown file type %s" % track_file) 
+    # Set final track options using all available sources
+    for k, v in def_opts.items() + guess_opts.items():
+        if k in valid_opts_list and not track_opts.has_key(k):
+            track_opts[k] = v
 
-    t = tracks.setdefault(track['type'], [])
-    t.append(track)
+    track = {
+        'type': track_type,
+        'file': track_file,
+        'opts': track_opts,
+    }
 
-#print repr(tracks) # FIXME
+    tracks.setdefault(track['type'], []).append(track)
+
+# print repr(tracks) # FIXME
 
 if not tracks.has_key('video'):
     fail('No video tracks')
@@ -112,6 +176,7 @@ uprint(['--output', dest_file])
 uprint(['--command-line-charset', 'utf-8'])
 #uprint(['--output-charset', 'utf-8'])
 #uprint(['--default-language', def_lang])
+
 # chapters
 if tracks.has_key('chapters'):
     tt = tracks['chapters']
@@ -119,15 +184,13 @@ if tracks.has_key('chapters'):
         fail("Only single chapters file allowed")
     t = tt[0]
     uprint(['--chapter-charset', 'utf-8'])
-    uprint(['--language', '-1:' + (t['lang'] if t.has_key('lang') else def_lang)])
+    print_options(t['opts'])
     uprint(['--chapters', t['file']])
 
 is_first_v_track = True
 for t in tracks['video']:
     if is_first_v_track:
-        if t.has_key('title'):
-            uprint(['--track-name', '-1:' + t['title']])
-	uprint(['--language', '-1:' + (t['lang'] if t.has_key('lang') else def_lang)])
+        print_options(t['opts'])
         uprint(['--default-track', '-1:1'])
         uprint([t['file']])
         is_first_v_track = False
@@ -138,25 +201,16 @@ is_first_a_track = True
 for t in tracks['audio']:
     if is_first_a_track:
         uprint(['--default-track', '-1:1'])
-	is_first_a_track = False
-    if t.has_key('title'):
-        uprint(['--track-name', '-1:' + t['title']])
-    if t.has_key('delay'):
-        uprint(['--sync', '-1:' + t['delay']])
-    if t['ext'] == '.mp4':
-        uprint(['--no-chapters'])
-    uprint(['--language', '-1:' + (t['lang'] if t.has_key('lang') else def_lang)])
+    is_first_a_track = False
+    print_options(t['opts'])
+    uprint(['--no-chapters'])
     uprint([t['file']])
 
 if tracks.has_key('subtitles'):
     for t in tracks['subtitles']:
         uprint(['--default-track', '-1:0'])
-        if t.has_key('title'):
-            uprint(['--track-name', '-1:' + t['title']])
-        if t.has_key('delay'):
-            uprint(['--sync', '-1:' + t['delay']])
-	uprint(['--language', '-1:' + (t['lang'] if t.has_key('lang') else def_lang)])
-        uprint(['--sub-charset', '-1:ucs-2'])
+        print_options(t['opts'])
+#        uprint(['--sub-charset', '-1:ucs-2le'])
         uprint([t['file']])
 
 """
